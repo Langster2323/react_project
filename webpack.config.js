73b0d0055fc8acb8b7b2e7bb02d11566e24bc944
Ahkeem Lang <langahkeem@gmail.com>
2017-06-03 14:10:14 -0700

    Configuring files

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,16 @@
-module.exports = {};
+module.exports = {
+  entry: __dirname + '/app/index.js',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/
+        exclude: /node_modules/
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  output: {
+    filename: 'transformed.js',
+    path: __dirname + '/build'
+  }
+};
