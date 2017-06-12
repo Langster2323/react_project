3262570aeb41e723afe6e48d04d3967cbb8f99d8
Ahkeem Lang <langahkeem@gmail.com>
2017-06-12 13:21:32 -0500

    Created an array of players. That held objects to get rid of the hard coded players within my players component. I also added a key to help the loop understand exactly which objects map.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
+var PLAYERS = [
+  {
+    name: "Nate",
+    score: 20,
+    id: 1,
+  },
+  {
+    name: "Cameron",
+    score: 20,
+    id: 2,
+  },
+  {
+    name: "Eric",
+    score: 20,
+    id: 3,
+  },
+  {
+    name: "Ahkeem",
+    score: 20,
+    id: 4,
+  }
+];
+
 function Header (props) {
   return (
     <div className="header">
@@ -36,16 +59,17 @@ function Player(props) {
 Player.propTypes = {
   name: React.PropTypes.string.isRequired,
   score: React.PropTypes.number.isRequired,
-}
+};
 
 function Application(props) {
   return (
     <div className="scoreboard">
       <Header title={props.title} />
-
+    //The key is helping the loop understand exactly which objects map to each virtual DOM elements
       <div className="players">
-        <Player name="Nate" score={20} />
-        <Player name="Ahkeem" score={20} />
+        {props.players.map(function(player) {
+          return <Player name={player.name} score={player.score} key={player.id}/>
+         })}
         </div>
       </div>
   );
@@ -54,6 +78,12 @@ function Application(props) {
 Application.propTypes = {
   //type definition
   title: React.PropTypes.string,
+  //Actual properties Application takes...
+  players: React.PropTypes.arrayOf(React.PropTypes.shape({
+    name: React.PropTypes.string.isRequired,
+    score: React.PropTypes.number.isRequired,
+    id: React.PropTypes.number.isRequired,
+  })).isRequired,
 };
 
 // To give a property a default value
@@ -61,4 +91,4 @@ Application.defaultProps = {
   title: "Scoreboard",
 }
 
-ReactDOM.render(<Application />, document.getElementById('container'));
+ReactDOM.render(<Application players={PLAYERS}/>, document.getElementById('container'));
