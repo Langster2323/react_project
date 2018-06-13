3612c51332c6a6453c890c330600beb509e919c9
Ahkeem Lang <langahkeem@gmail.com>
2018-06-13 11:16:21 -0500

    Updated import

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App, { PLAYERS } from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<App initialPlayers={PLAYERS}/>, document.getElementById('root'));
 registerServiceWorker();
