28a0983567d14e4eb466a9cea439931536e0687e
Ahkeem Lang <langahkeem@gmail.com>
2018-08-01 20:55:59 -0500

    Refactored player function. aligned a dispatch for actions

--- my-app/src/containers/Scoreboard.js
+++ my-app/src/containers/Scoreboard.js
@@ -1,4 +1,6 @@
 import React, { Component, PropTypes } from 'react';
+import { bindActionsCreators } from 'redux';
+import * as PlayerActionCreators from '../actions/players';
 import { connect } from 'react-redux';
 import '../App.css';
 
@@ -7,24 +9,35 @@ import AddPlayerForm from '../components/add-player-form';
 import Header from '../components/header';
 
 class Scoreboard extends Component {
+
+  static propTypes = {
+    players: PropTypes.array.isRequired
+  }
+
   render() {
+    const { dispatch, players } = this.props;
+    const addPlayer = bindActionsCreators(PlayerActionCreators.addPlayer, dispatch);
+    const removePlayer = bindActionsCreators(PlayerActionCreators.removePlayer, dispatch);
+    const updatePlayerScore = bindActionsCreators(PlayerActionCreators.updatePlayerScorePlayer, dispatch);
+
+    const playerComponents = players.map((player, index) =>
+      <Player
+        index={index}
+        name={player.name}
+        score={player.score}
+        key={player.name}
+        updatePlayerScore={updatePlayerScore}
+        removePlayer={removePlayer}
+       />
+  );
+
     return (
       <div className="scoreboard">
-        <Header players={this.state.players} />
+        <Header players={players} />
         <div className="players">
-          {this.state.players.map(function(player, index) {
-             return (
-               <Player
-                 name={player.name}
-                 score={player.score}
-                 key={player.name}
-                 onScoreChange={(delta) => this.onScoreChange(index, delta)}
-                 onRemove={() => this.onRemovePlayer(index)}
-               />
-             );
-           }.bind(this))}
+          { playerComponents }
         </div>
-        <AddPlayerForm onAdd={this.onAddPlayer} />
+        <AddPlayerForm addPlayer={addPlayer} />
       </div>
     );
   }
